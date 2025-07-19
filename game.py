c7a90613285d1a8a531849f2c5671aadc2df1088
skier921 <reedg921@gmail.com>
2025-07-19 03:42:06 +0000

    Game development with TDD

---
+++ game.py
@@ -0,0 +1,25 @@
+import string
+import random
+
+class Game:
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase)) 
+        pass
+
+    def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy() # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
+    
+game = Game()
+print(game.grid) # --> OQUWRBAZE
+my_word = "BAROQUE"
+game.is_valid(my_word) # --> True
