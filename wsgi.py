7e030228adb87d38da26565f283dc5b02800013b
skier921 <reedg921@gmail.com>
2025-07-19 04:17:17 +0000

    Google cloud app

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from game import Game
+from wsgi import app
 
 app = Flask(__name__)
 
@@ -15,3 +16,8 @@ def check():
     word = request.form['word']
     is_valid = game.is_valid(word)
     return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word)
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process will serve the app
+    app.run(host='127.0.0.1', port=8080, debug=True)
